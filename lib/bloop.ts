66f3f58f4d6561ffd28af7a734a92c39de59fc24
ae <andy@1001harps.com>
2023-03-14 13:35:37 +0000

    move Bloop class to own file

---
+++ lib/bloop.ts
@@ -0,0 +1,50 @@
+import { clamp } from "./math";
+import { getMidiOutput, triggerNote } from "./midi";
+import { parsePatch } from "./parser";
+import { AsyncResult, Err, Ok } from "./types";
+
+export class Bloop {
+  bpm = 120;
+  bpms = this.bpm / 60 / 1000;
+  interval = 1 / this.bpms / 4;
+  currentStep = 0;
+
+  async playPatch(patch: string): AsyncResult<boolean, string> {
+    const parsedPatch = parsePatch(patch);
+    if (!parsedPatch.ok) {
+      return Err(parsedPatch.error);
+    }
+
+    const midiOutput = await getMidiOutput();
+
+    const interval = setInterval(() => {
+      const patterns = parsedPatch.value.patterns;
+
+      // get length of longest pattern
+      const sequenceLength = Math.max(...patterns.map((x) => x.steps.length));
+
+      for (let p of patterns) {
+        const i = this.currentStep % p.steps.length;
+
+        const step = p.steps[i];
+        if (step.type === "NOTE") {
+          try {
+            const note = p.settings.octave * 12 + step.note;
+            const clampedNote = clamp(note, 0, 127);
+            triggerNote(midiOutput, p.channel, clampedNote);
+          } catch (error) {
+            console.error(error);
+            clearInterval(interval);
+          }
+        }
+      }
+
+      this.currentStep++;
+      if (this.currentStep >= sequenceLength) {
+        this.currentStep = 0;
+      }
+    }, this.interval);
+
+    return Ok(true);
+  }
+}
